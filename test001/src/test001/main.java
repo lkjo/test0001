14bc44e1a42e49a0d42111044cd7f239caa93a15
lkjo <tryit163281@gmail.com>
2021-08-17 11:31:31 +0800

    01

---
+++ test001/src/test001/main.java
@@ -0,0 +1,39 @@
+package test001;
+
+public class main {
+
+	public static void main(String[] args) {
+//		System.out.println("Hello world");
+//		//俱计
+//		int x,y,z;//Τ俱计x,y,z
+//		x = 10;//–︽常穦Τだ腹
+//		y = 5;
+//		z = 8;
+//		//print()セōぃ穦传︽
+//		//println()传︽
+//		// "\n"琌传︽才腹穦传︽
+//		System.out.println("\n");
+//		System.out.println(x+y);
+//		System.out.println(x-y);
+//		System.out.println(x*y);
+//		System.out.println(x/y);
+//		String text = "HELLO WORLD";
+//		//String is not a command type in Java.
+//		System.out.print(text);
+		
+		//loop 癹伴 if I want to do something many times, I should to use that.
+		//for loop
+		for (int i = 0;i < 10;i = i + 1) {
+			//First you have to define a variable 
+			//and then you should tell it, it can do ?? times
+			//variable will plus 1 may you run one time
+			System.out.println(i);
+			//int i = 0; set the initial number
+			//i < 10; the range for i (0 ~ 10)
+			//i < 100;(0 ~ 100) i < 1000;(0~1000)
+			//i = i + 1; when run the code one time, it would plused one.
+			//i = i + 2; when run the code one time, it would plused two.
+		}
+	}
+
+}
---
+++ test001/src/test001/sec.java
@@ -0,0 +1,10 @@
+package test001;
+
+public class sec {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
